5bbc766c062f3e72f82f97679287adf8606eefd2
INWELLCOM\lisayueh <lisayueh@dayongtek.com>
2025-12-02 15:50:02 +0800

    Update path in push_changes.bat to NewsCommentApp

--- push_changes.bat
+++ push_changes.bat
@@ -3,7 +3,7 @@ REM 設定 Git 自動轉換換行符號
 git config --global core.autocrlf true
 
 REM 進入你的專案資料夾（可修改成實際路徑）
-cd /d C:\Work_n8n\Daily_news\newsApp
+cd /d C:\Work_n8n\Daily_news\NewsCommentApp
 
 REM 將所有修改加入暫存區
 git add .
